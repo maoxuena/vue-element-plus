4afe3e53eee77491f44b335df1af15bf10d5fbae
maoxuena <maoxuena@foxmail.com>
2021-09-28 13:43:41 +0800

    leetcode 增加方法

--- src/utils/leetcode.js
+++ src/utils/leetcode.js
@@ -17,4 +17,24 @@
     }
 };
 
-export {twoSum}
+
+/**
+ * 136. 只出现一次的数字
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 思路：
+ * 采用异或运算，数组中只有一个数出现了一次，其他的都出现了两次
+ * 通过异或，把所有出现偶数次的数字给消除，最后剩下的只有那一个出现一次的数字
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const singleNumber = function(nums) {
+  let res = 0;
+    for(const num of nums) {
+        res ^= num;
+    }
+    return res;
+};
+
+export {twoSum, singleNumber}
